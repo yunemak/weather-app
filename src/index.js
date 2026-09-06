cf8f59554fe211b23abced0ab65673e253c7ed2a
yunemak <yeak558@gmail.com>
2026-09-06 13:52:03 +0300

    feat: Fetch json object from Visual Crossing

--- src/index.js
+++ src/index.js
@@ -6,3 +6,15 @@ const apiKey = "WWFDD6U4G65S9P5GNQAEYDYDV";
 const location = "Ankara";
 
 let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${apiKey}&contentType=json`;
+
+getWeather();
+
+async function getWeather() {
+	try {
+		let response = await fetch(url);
+		let json = await response.json();
+		console.log(json);
+	} catch (error) {
+		console.log("Some error occured");
+	}
+}
