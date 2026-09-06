ce582018f26bfef264aebc55aa3b006808b3cc15
yunemak <yeak558@gmail.com>
2026-09-06 13:48:55 +0300

    feat: Basic variables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import "./style.css";
 
 console.log("deneme");
+
+const apiKey = "WWFDD6U4G65S9P5GNQAEYDYDV";
+const location = "Ankara";
+
+let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${apiKey}&contentType=json`;
