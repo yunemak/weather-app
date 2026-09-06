7637ef3a5eeca30daa2f7139bdee9a4fa4174672
yunemak <yeak558@gmail.com>
2026-09-06 14:09:09 +0300

    feat: Catch errors, try another city

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import "./style.css";
 console.log("deneme");
 
 const apiKey = "WWFDD6U4G65S9P5GNQAEYDYDV";
-const location = "Ankara";
 
-let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${apiKey}&contentType=json`;
+getWeather("Ankaraaaaaaaa");
+getWeather("İstanbul");
 
-getWeather();
-
-async function getWeather() {
+async function getWeather(location) {
 	try {
+		let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${apiKey}&contentType=json`;
 		let response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(`The city ${location} cannot be found!`);
+		}
 		let json = await response.json();
 		console.log(`Şehir adı: ${json.resolvedAddress}`);
 		console.log(`Anlık sıcaklık: ${json.currentConditions.temp}`);
@@ -19,6 +21,6 @@ async function getWeather() {
 		console.log(`Bugün en yüksek sıcaklık: ${json.days[0].tempmax}`);
 		console.log(`Bugün en düşük sıcaklık: ${json.days[0].tempmin}`);
 	} catch (error) {
-		console.log("Some error occured");
+		console.log(`Error: ${error.message}`);
 	}
 }
